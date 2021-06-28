4df8b6bc34ba7b51f3e9e0ad40960837c2630134
nerdgit <winkore@gmail.com>
2021-06-28 16:56:48 +0800

    add an object for greeting

--- main.c
+++ main.c
@@ -3,7 +3,13 @@
 
 int main(int argc, char** argv) {
 
-    printf("Hello, World!\n");
+    char* obj = "";
+
+    if (argc > 1) {
+        obj = argv[1];
+    }
+
+    printf("Hello, World! %s!\n", obj);
 
     return 0;
 }
