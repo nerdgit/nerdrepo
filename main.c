148438d381a845e2008f9f01856662c041c8ece2
nerdgit <winkore@gmail.com>
2021-06-28 19:31:40 +0800

    update main.c

--- main.c
+++ main.c
@@ -3,13 +3,13 @@
 
 int main(int argc, char** argv) {
 
-    char* obj = "";
+    char* obj = "World";
 
     if (argc > 1) {
         obj = argv[1];
     }
 
-    printf("Hello, World! %s!\n", obj);
+    printf("Hello, %s!\n", obj);
 
     return 0;
 }
